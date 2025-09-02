2aa9e34ec603cf3724a9066a6f2a8271e959a7a6
yonathancruz <yonathancruz2019@gmail.com>
2025-09-02 13:49:37 -0600

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   vite: {
     plugins: [tailwindcss()],
   },
 
   integrations: [react()],
+  adapter: vercel(),
 });
