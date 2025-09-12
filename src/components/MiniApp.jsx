f9d1a08245f600f2b11ae36d5bf8b505807594ba
yonathancruz <yonathancruz2019@gmail.com>
2025-09-12 11:07:18 -0600

    remove await

--- src/components/MiniApp.jsx
+++ src/components/MiniApp.jsx
@@ -17,7 +17,7 @@ export default function MiniApp() {
         await sdk.actions.ready();
         console.log("Farcaster MiniApp SDK is ready!");
         window.isFarcaster = true;
-        const profile = await sdk.context.user;
+        const profile = sdk.context.user;
 
         if (profile) {
           console.log(profile);
