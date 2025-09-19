99f1f87c7b747ae0a7ca9e02c06df2c2732f9a19
yonathancruz <yonathancruz2019@gmail.com>
2025-09-18 19:19:06 -0600

    create smart contract wallet

--- src/pages/api/wallets/create-wallet.json.js
+++ src/pages/api/wallets/create-wallet.json.js
@@ -4,14 +4,13 @@ const CDP_API_KEY_ID = import.meta.env.CDP_API_KEY_ID;
 const CDP_API_KEY_SECRET = import.meta.env.CDP_API_KEY_SECRET;
 const CDP_WALLET_SECRET = import.meta.env.CDP_WALLET_SECRET;
 
-export default async function POST() {
+export default async function POST({ request }) {
+  const { account } = await request.json();
   const cdp = new CdpClient({
     apiKeyId: CDP_API_KEY_ID,
     apiKeySecret: CDP_API_KEY_SECRET,
     walletSecret: CDP_WALLET_SECRET,
   });
-
-  const account = await cdp.evm.createAccount();
 
   const smartAccount = await cdp.evm.createSmartAccount({
     owner: account,
